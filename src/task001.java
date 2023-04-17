f7fabc380bd751509fddfb55fa660bd4f0686f40
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2023-04-17 13:29:59 +0300

    v001

--- src/task001.java
+++ src/task001.java
@@ -1,5 +1,17 @@
 public class task001 {
     public static void main(String[] args) {
-        System.out.println("hello wold");
+        System.out.println("hello wold new");
+        int [] arr=new int[] {2,6,9,7};
+//
+//            for (int i=0;i<10;i++) {
+//            if (i%2!=0) {
+//                continue;
+//            }
+                for (int item:
+                     arr) {
+                    System.out.println(item);
+                }
+//            System.out.println(i);
+
     }
 }
